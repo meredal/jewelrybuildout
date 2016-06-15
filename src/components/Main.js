bf5ad7c19bd89e714cc8607505aafa4ef5397eef
meredal <mldalenburg@gmail.com>
2016-06-15 15:22:16 +0700

    Add Reel Component for Slideshow

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,44 +3,15 @@ require('styles/App.scss');
 
 import React from 'react';
 import ArrowComponent from './slideshow/ArrowComponent.js';
-import SlideComponent from './slideshow/SlideComponent.js';
-
-const slideData = [
-  {
-    key: 'slide1',
-    imagePath: 'images/ring.jpg',
-    imageAlt: 'Diamond Ring',
-    title: 'Mercury'
-  },
-  {
-    key: 'slide2',
-    imagePath: 'http://placeimg.com/400/400/nature/grayscale',
-    imageAlt: 'Dummy Image 1',
-    title: 'Venus'
-  },
-  {
-    key: 'slide3',
-    imagePath: 'http://placeimg.com/400/400/nature/grayscale',
-    imageAlt: 'Dummy Image 2',
-    title: 'Jupiter'
-  }
-]
+import ReelComponent from './slideshow/ReelComponent.js';
 
 class AppComponent extends React.Component {
-  renderSlides() {
-    return slideData.map(
-      (slideProps) => (<SlideComponent {...slideProps} />)
-    );
-  }
-
   render() {
     return (
       <div id="work" className="slideshow">
         <ArrowComponent direction="left" />
         <ArrowComponent direction="right" />
-        <div className="reel">
-          {this.renderSlides()}
-        </div>
+        <ReelComponent />
       </div>
     );
   }
---
+++ src/components/slideshow/ReelComponent.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import React from 'react';
+import SlideComponent from './SlideComponent.js';
+
+require('styles/slideshow/Reel.scss');
+
+const slideData = [
+  {
+    key: 'slide1',
+    imagePath: 'images/ring.jpg',
+    imageAlt: 'Diamond Ring',
+    title: 'Mercury'
+  },
+  {
+    key: 'slide2',
+    imagePath: 'http://placeimg.com/400/400/nature/grayscale',
+    imageAlt: 'Dummy Image 1',
+    title: 'Venus'
+  },
+  {
+    key: 'slide3',
+    imagePath: 'http://placeimg.com/400/400/nature/grayscale',
+    imageAlt: 'Dummy Image 2',
+    title: 'Jupiter'
+  }
+]
+
+let renderSlides = function() {
+  return slideData.map(
+    (slideProps) => (<SlideComponent {...slideProps} />)
+  );
+}
+
+
+let ReelComponent = () => (
+  <div className="reel-component">
+    {renderSlides()}
+  </div>
+);
+
+ReelComponent.displayName = 'SlideshowReelComponent';
+
+// Uncomment properties you need
+// ReelComponent.propTypes = {};
+// ReelComponent.defaultProps = {};
+
+export default ReelComponent;
